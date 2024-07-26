d9300c5b39ad0289e775030597d48c522f5aca3e
Francois Qian <francois.qian2@gmail.com>
2024-07-26 21:43:40 +0100

    message

--- src/finger.cpp
+++ src/finger.cpp
@@ -8,19 +8,19 @@ class Finger
 public: 
     int id;
     double distanceTravelled;
-    bool isPressed;
+    bool isUsed;
     double dexterity;
     double currentX, currentY;
     double homeX, homeY;
 
-    Finger(int id, double homex, double homeY, double dexterity) : id(id), distanceTravelled(0), isPressed(false), dexterity(dexterity), currentX(homeX), currentY(homeY), homeX(homeX), homeY(homeY) {}
+    Finger(int id, double homex, double homeY, double dexterity) : id(id), distanceTravelled(0), isUsed(false), dexterity(dexterity), currentX(homeX), currentY(homeY), homeX(homeX), homeY(homeY) {}
 
     void press(const Key& key)
     {
         double dist = std::sqrt(std::pow(key.x - currentX, 2) + std::pow(key.y - currentY, 2));
-        distanceTravelled += dist;
+        distanceTravelled += dist * (1/dexterity) * isUsed ? 2 : 1;
         currentX = key.x;
         currentY = key.y;
-        isPressed = true;
+        isUsed = true;
     }
 };
